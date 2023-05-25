12c79571eecbc6ebc024b2d3031e5e448a960cc6
Andrey Azevedo <mechamoandrey@gmail.com>
2023-05-25 01:38:23 -0300

    feature: filtering on input

--- src/js/modules/searchStart.js
+++ src/js/modules/searchStart.js
@@ -1,3 +1,5 @@
+import { state } from './../states'
+
 const debounce = (func, timeout = 500) => {
   let timer;
   return (...args) => {
@@ -6,14 +8,34 @@ const debounce = (func, timeout = 500) => {
   };
 }
 
-const saveInput = () => {
-  console.log('Saving data');
+const saveInput = (event) => {
+  const filteredResult = []
+  const input = event.target
+  let inputValue = input.value
+
+  // Remove caracteres especiais usando expressão regular
+  inputValue = inputValue.replace(/[^\D\s]/gi, '')
+
+  // Atualiza o valor do input com o texto filtrado
+  input.value = inputValue
+
+  // Filtro
+  for (let index = 0; index < state.allCountries.length; index++) {
+    const elementCountry = state.allCountries[index];
+    const nameLower = elementCountry.name.common.toLowerCase()
+    const nameCapitalized = elementCountry.name.common
+    
+    if(nameLower.includes(inputValue) || nameCapitalized.includes(inputValue)) {
+      filteredResult.push(elementCountry)
+    }
+  }
+  state.listResult = filteredResult
 }
 
 export const searchStart = () => {
   const searchInput = document.querySelector('._search')
 
-  const processChanges = debounce(() => saveInput());
+  const processChanges = debounce((e) => saveInput(e))
 
-  searchInput.addEventListener("keyup", processChanges)
+  searchInput.addEventListener("keydown", processChanges)
 }
